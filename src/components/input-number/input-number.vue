ca7ec7fcb9bd4a03124103b9010fe6a7afae132e
mars <mars.yang@wisdom56.com>
2018-10-30 19:54:42 +0800

    inputnumber bug

--- src/components/input-number/input-number.vue
+++ src/components/input-number/input-number.vue
@@ -254,7 +254,7 @@
             },
             setValue (val, isInt) {
                 // 如果 step 是小数，且没有设置 precision，是有问题的
-                if (val && !isNaN(this.precision) && isInt === undefined) val = new BigNumber(val.toString()).toFixed(this.precision, 1);
+                if (val && !isNaN(this.precision) && isInt === undefined) val = Number(new BigNumber(val.toString()).toFixed(this.precision, 1));
                 this.$nextTick(() => {
                     this.currentValue = val;
                     this.$emit('input', val);
