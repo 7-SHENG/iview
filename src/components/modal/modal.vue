fe3b9f680268ce1e51e221aa85bc12415d0e6957
7126163 <ryan.sheng@wisdom56.com>
2019-02-21 10:05:47 +0800

    更换为原版的modal拖动

--- src/components/modal/modal.vue
+++ src/components/modal/modal.vue
@@ -1,18 +1,21 @@
 <template>
     <div v-transfer-dom :data-transfer="transfer">
         <transition :name="transitionNames[1]">
-            <div :class="maskClasses" v-show="visible" @click="mask"></div>
+            <div :class="maskClasses" :style="wrapStyles" v-show="visible" v-if="showMask" @click="handleMask"></div>
         </transition>
-        <div :class="wrapClasses" @click="handleWrapClick" @mousemove="mouseMove" @mouseup="mouseUp">
+        <div :class="wrapClasses" :style="wrapStyles" @click="handleWrapClick">
             <transition :name="transitionNames[0]" @after-leave="animationFinish">
                 <div :class="classes" :style="mainStyles" v-show="visible">
-                    <div :class="[prefixCls + '-content']" :style="{left: currentPosition.x + 'px', top: currentPosition.y + 'px'}">
+                    <div :class="contentClasses" ref="content" :style="contentStyles" @click="handleClickModal">
                         <a :class="[prefixCls + '-close']" v-if="closable" @click="close">
                             <slot name="close">
-                                <Icon type="ios-close-empty"></Icon>
+                                <Icon type="ios-close"></Icon>
                             </slot>
                         </a>
-                        <div :class="[prefixCls + '-header']" ondragstart="return false;" @mousedown="mouseDown" @mouseup="mouseUp" v-if="showHead"><slot name="header"><div :class="[prefixCls + '-header-inner']">{{ title }}</div></slot></div>
+                        <div :class="[prefixCls + '-header']"
+                             @mousedown="handleMoveStart"
+                             v-if="showHead"
+                        ><slot name="header"><div :class="[prefixCls + '-header-inner']">{{ title }}</div></slot></div>
                         <div :class="[prefixCls + '-body']"><slot></slot></div>
                         <div :class="[prefixCls + '-footer']" v-if="!footerHide">
                             <slot name="footer">
@@ -33,9 +36,10 @@
     import Locale from '../../mixins/locale';
     import Emitter from '../../mixins/emitter';
     import ScrollbarMixins from './mixins-scrollbar';
-
+    import { on, off } from '../../utils/dom';
+    import { findComponentsDownward } from '../../utils/assist';
+    import { transferIndex as modalIndex, transferIncrease as modalIncrease } from '../../utils/transfer-queue';
     const prefixCls = 'ivu-modal';
-
     export default {
         name: 'Modal',
         mixins: [ Locale, Emitter, ScrollbarMixins ],
@@ -94,8 +98,26 @@
             },
             transfer: {
                 type: Boolean,
+                default () {
+                    return !this.$IVIEW || this.$IVIEW.transfer === '' ? true : this.$IVIEW.transfer;
+                }
+            },
+            fullscreen: {
+                type: Boolean,
+                default: false
+            },
+            mask: {
+                type: Boolean,
                 default: true
-            }
+            },
+            draggable: {
+                type: Boolean,
+                default: false
+            },
+            zIndex: {
+                type: Number,
+                default: 1000
+            },
         },
         data () {
             return {
@@ -104,19 +126,14 @@
                 showHead: true,
                 buttonLoading: false,
                 visible: this.value,
-                position: {
-                    x: 0,
-                    y: 0
+                dragData: {
+                    x: null,
+                    y: null,
+                    dragX: null,
+                    dragY: null,
+                    dragging: false
                 },
-                startPosition: {
-                    x: 0,
-                    y: 0
-                },
-                currentPosition: {
-                    x: 0,
-                    y: 0
-                },
-                moveFlag: false
+                modalIndex: this.handleGetModalIndex(),  // for Esc close the top modal
             };
         },
         computed: {
@@ -125,28 +142,62 @@
                     `${prefixCls}-wrap`,
                     {
                         [`${prefixCls}-hidden`]: !this.wrapShow,
-                        [`${this.className}`]: !!this.className
+                        [`${this.className}`]: !!this.className,
+                        [`${prefixCls}-no-mask`]: !this.showMask
                     }
                 ];
+            },
+            wrapStyles () {
+                return {
+                    zIndex: this.modalIndex + this.zIndex
+                };
             },
             maskClasses () {
                 return `${prefixCls}-mask`;
             },
             classes () {
-                return `${prefixCls}`;
+                return [
+                    `${prefixCls}`,
+                    {
+                        [`${prefixCls}-fullscreen`]: this.fullscreen,
+                        [`${prefixCls}-fullscreen-no-header`]: this.fullscreen && !this.showHead,
+                        [`${prefixCls}-fullscreen-no-footer`]: this.fullscreen && this.footerHide
+                    }
+                ];
+            },
+            contentClasses () {
+                return [
+                    `${prefixCls}-content`,
+                    {
+                        [`${prefixCls}-content-no-mask`]: !this.showMask,
+                        [`${prefixCls}-content-drag`]: this.draggable,
+                        [`${prefixCls}-content-dragging`]: this.draggable && this.dragData.dragging
+                    }
+                ];
             },
             mainStyles () {
                 let style = {};
-
                 const width = parseInt(this.width);
-                const styleWidth = {
+                const styleWidth = this.dragData.x !== null ? {
+                    top: 0
+                } : {
                     width: width <= 100 ? `${width}%` : `${width}px`
                 };
-
                 const customStyle = this.styles ? this.styles : {};
-
                 Object.assign(style, styleWidth, customStyle);
-
+                return style;
+            },
+            contentStyles () {
+                let style = {};
+                if (this.draggable) {
+                    if (this.dragData.x !== null) style.left = `${this.dragData.x}px`;
+                    if (this.dragData.y !== null) style.top = `${this.dragData.y}px`;
+                    const width = parseInt(this.width);
+                    const styleWidth = {
+                        width: width <= 100 ? `${width}%` : `${width}px`
+                    };
+                    Object.assign(style, styleWidth);
+                }
                 return style;
             },
             localeOkText () {
@@ -162,43 +213,26 @@
                 } else {
                     return this.cancelText;
                 }
+            },
+            showMask () {
+                return this.draggable ? false : this.mask;
             }
         },
         methods: {
-            mouseDown(e) {
-                this.startPosition.x = e.screenX;
-                this.startPosition.y = e.screenY;
-                this.moveFlag = true;
-            },
-            mouseMove(e) {
-                if(this.moveFlag) {
-                    this.currentPosition.x = this.position.x + (e.screenX - this.startPosition.x);
-                    this.currentPosition.y = this.position.y + (e.screenY - this.startPosition.y);
-                }
-            },
-            mouseUp(e) {
-                if(this.moveFlag) {
-                    this.moveFlag = false;
-                    this.currentPosition.x = this.position.x + (e.screenX - this.startPosition.x);
-                    this.currentPosition.y = this.position.y + (e.screenY - this.startPosition.y);
-                    this.position.x = this.currentPosition.x;
-                    this.position.y = this.currentPosition.y;
-                }
-            },
             close () {
                 this.visible = false;
                 this.$emit('input', false);
                 this.$emit('on-cancel');
             },
-            mask () {
-                if (this.maskClosable) {
+            handleMask () {
+                if (this.maskClosable && this.showMask) {
                     this.close();
                 }
             },
             handleWrapClick (event) {
                 // use indexOf,do not use === ,because ivu-modal-wrap can have other custom className
                 const className = event.target.getAttribute('class');
-                if (className && className.indexOf(`${prefixCls}-wrap`) > -1) this.mask();
+                if (className && className.indexOf(`${prefixCls}-wrap`) > -1) this.handleMask();
             },
             cancel () {
                 this.close();
@@ -215,27 +249,74 @@
             EscClose (e) {
                 if (this.visible && this.closable) {
                     if (e.keyCode === 27) {
-                        this.close();
+                        const $Modals = findComponentsDownward(this.$root, 'Modal').filter(item => item.$data.visible && item.$props.closable);
+                        const $TopModal = $Modals.sort((a, b) => {
+                            return a.$data.modalIndex < b.$data.modalIndex ? 1 : -1;
+                        })[0];
+                        setTimeout(() => {
+                            $TopModal.close();
+                        }, 0);
                     }
                 }
             },
             animationFinish() {
                 this.$emit('on-hidden');
+            },
+            handleMoveStart (event) {
+                if (!this.draggable) return false;
+                const $content = this.$refs.content;
+                const rect = $content.getBoundingClientRect();
+                this.dragData.x = rect.x || rect.left;
+                this.dragData.y = rect.y || rect.top;
+                const distance = {
+                    x: event.clientX,
+                    y: event.clientY
+                };
+                this.dragData.dragX = distance.x;
+                this.dragData.dragY = distance.y;
+                this.dragData.dragging = true;
+                on(window, 'mousemove', this.handleMoveMove);
+                on(window, 'mouseup', this.handleMoveEnd);
+            },
+            handleMoveMove (event) {
+                if (!this.dragData.dragging) return false;
+                const distance = {
+                    x: event.clientX,
+                    y: event.clientY
+                };
+                const diff_distance = {
+                    x: distance.x - this.dragData.dragX,
+                    y: distance.y - this.dragData.dragY
+                };
+                this.dragData.x += diff_distance.x;
+                this.dragData.y += diff_distance.y;
+                this.dragData.dragX = distance.x;
+                this.dragData.dragY = distance.y;
+            },
+            handleMoveEnd () {
+                this.dragData.dragging = false;
+                off(window, 'mousemove', this.handleMoveMove);
+                off(window, 'mouseup', this.handleMoveEnd);
+            },
+            handleGetModalIndex () {
+                modalIncrease();
+                return modalIndex;
+            },
+            handleClickModal () {
+                if (this.draggable) {
+                    this.modalIndex = this.handleGetModalIndex();
+                }
             }
         },
         mounted () {
             if (this.visible) {
                 this.wrapShow = true;
             }
-
             let showHead = true;
-
             if (this.$slots.header === undefined && !this.title) {
                 showHead = false;
             }
-
             this.showHead = showHead;
-
             // ESC close
             document.addEventListener('keydown', this.EscClose);
         },
@@ -255,6 +336,7 @@
                         this.removeScrollEffect();
                     }, 300);
                 } else {
+                    this.modalIndex = this.handleGetModalIndex();
                     if (this.timer) clearTimeout(this.timer);
                     this.wrapShow = true;
                     if (!this.scrollable) {
